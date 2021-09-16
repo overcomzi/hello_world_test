e308f79f0c06bf7e6acc898f6334b6a9c9881e7f
Overcomzi <ilya_zelentsov23@inbox.ru>
2021-09-16 13:07:05 +0300

    Добавил символ перевода строки в строку hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, world!1");
+        System.out.println("Hello, world!1\n");
         System.out.println("New Feature!");
     }
 }
